df730634d74326e1813c35d1ec8b96525f4e604f
notoiro297 <notoiro297@gmail.com>
2024-07-26 03:56:41 +0900

    add port option

--- main.go
+++ main.go
@@ -5,32 +5,34 @@ import (
   "encoding/json"
   "net/http"
   "log"
+  "flag"
+  "strconv"
 
   "github.com/ikawaha/kagome-dict-ipa-neologd"
-	"github.com/ikawaha/kagome/v2/tokenizer"
+  "github.com/ikawaha/kagome/v2/tokenizer"
 )
 
 type TokenizeHandler struct{}
 type TokenizerRequestBody struct {
-	Input string `json:"text"`
+  Input string `json:"text"`
 }
 type TokenizerResponseBody struct {
-	Tokens []tokenizer.TokenData `json:"tokens"`
+  Tokens []tokenizer.TokenData `json:"tokens"`
 }
 
 func igOK(s string, _ bool) string {
-	return s
+  return s
 }
 
 func tokenize(str string) []tokenizer.TokenData {
-	if len(str) == 0 {
-		return nil
-	}
-	t, err := tokenizer.New(ipaneologd.Dict(), tokenizer.OmitBosEos())
-	if err != nil {
-		return nil
-	}
-	tokens := t.Tokenize(str)
+  if len(str) == 0 {
+    return nil
+  }
+  t, err := tokenizer.New(ipaneologd.Dict(), tokenizer.OmitBosEos())
+  if err != nil {
+    return nil
+  }
+  tokens := t.Tokenize(str)
   var tokenData []tokenizer.TokenData
   for _, v := range tokens{
     tokenData = append(tokenData, tokenizer.NewTokenData(v))
@@ -40,45 +42,50 @@ func tokenize(str string) []tokenizer.TokenData {
 
 func (h *TokenizeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
   w.Header().Set("Content-Type", "application/json")
-	var req TokenizerRequestBody
+  var req TokenizerRequestBody
   if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, fmt.Sprintf("{\"error\":\"%v\"}", err), http.StatusBadRequest)
-		return
-	}
+    http.Error(w, fmt.Sprintf("{\"error\":\"%v\"}", err), http.StatusBadRequest)
+    return
+  }
 
   if req.Input == "" {
-		w.Write([]byte(`{"tokens":[]}`))
-		return
-	}
+    w.Write([]byte(`{"tokens":[]}`))
+    return
+  }
 
   tokens := tokenize(req.Input)
 
-	resp, err := json.Marshal(TokenizerResponseBody{
-		Tokens: tokens,
-	})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("{\"status\":false,\"error\":\"%v\"}", err), http.StatusInternalServerError)
-		return
-	}
+  resp, err := json.Marshal(TokenizerResponseBody{
+    Tokens: tokens,
+  })
+  if err != nil {
+    http.Error(w, fmt.Sprintf("{\"status\":false,\"error\":\"%v\"}", err), http.StatusInternalServerError)
+    return
+  }
   w.WriteHeader(http.StatusOK)
-	w.Write(resp)
+  w.Write(resp)
 }
 
-func start_server() {
+func start_server(port string) {
   mux := http.NewServeMux()
   mux.Handle("/tokenize", &TokenizeHandler{})
 
   srv := http.Server{
-    Addr: ":2971",
+    Addr: ":" + port,
     Handler: mux,
   }
 
-  log.Println("localhost:2971")
+  log.Println(port);
+  log.Println("localhost:" + port)
   srv.ListenAndServe()
 }
 
 func main() {
-  start_server()
+  var portFlag = flag.Int("port", 2971, "server port")
+  flag.Parse()
+
+  var port = strconv.Itoa(*portFlag)
+  start_server(port)
   println("Start...")
 }
 
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 # Kagome Front
 依存関係どうにかしたり`go build main.go`したり`./main`したり`127.0.0.1:2971/tokenize`に`{"text": "文字文字"}`投げつけて
+
+## option
+```
+--port INT
+  server port(default 2971)
+```
